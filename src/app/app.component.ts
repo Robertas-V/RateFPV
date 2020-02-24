d0a552ed411e976b98cf0692e3cea77d5236c873
Robertas-V <robertas.visniakovas@gmail.com>
2020-02-25 00:12:01 +0200

    Added one way communication to table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { AngularFirestore } from "@angular/fire/firestore";
 import {
   faPlus,
   faMinus,
   faHeart,
   faHeartBroken
 } from "@fortawesome/free-solid-svg-icons";
-import { ItemService } from "./core/services/item/item.service";
-import { Observable } from "rxjs";
-
-export interface Item {
-  name: string;
-  mainImageURL: string;
-  rating: number;
-  raters: number;
-}
 
 @Component({
   selector: "app-root",
@@ -27,21 +17,7 @@ export class AppComponent implements OnInit {
   faMinus = faMinus;
   faHeart = faHeart;
   faHeartBroken = faHeartBroken;
+  constructor() {}
 
-  // items: Item[] = [];
-  items: Observable<Item[]>;
-  constructor(private itemService: ItemService) {}
-  ngOnInit() {
-    this.items = this.itemService.getCameras();
-    // this.itemService.getCameras().subscribe((res: Item[]) => {
-    //   res.map(e => {
-    //     return {
-    //       name: e.name,
-    //       mainImageURL: e.mainImageURL,
-    //       rating: e.rating,
-    //       raters: e.raters
-    //     } as Item;
-    //   });
-    // });
-  }
+  ngOnInit() {}
 }
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { ItemService } from "src/app/core/services/item/item.service";
+import { Observable } from "rxjs";
+import {
+  faPlus,
+  faMinus,
+  faHeart,
+  faHeartBroken
+} from "@fortawesome/free-solid-svg-icons";
+export interface Item {
+  name: string;
+  mainImageURL: string;
+  rating: number;
+  raters: number;
+}
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.less']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.less"]
 })
 export class TableComponent implements OnInit {
+  faPlus = faPlus;
+  faMinus = faMinus;
+  faHeart = faHeart;
+  faHeartBroken = faHeartBroken;
+  // items: Item[] = [];
+  items: Observable<Item[]>;
 
-  constructor() { }
+  @Input() category: string;
 
+  constructor(private itemService: ItemService) {}
   ngOnInit() {
+    console.log(this.category);
+    this.items = this.itemService.getCameras();
   }
-
 }
