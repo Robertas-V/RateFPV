372ce20adf140202f2cc09b4ae222529e84001dc
Robertas-V <robertas.visniakovas@gmail.com>
2020-03-04 22:27:55 +0200

    Added rainbow colors

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -26,11 +26,50 @@ export class TableComponent implements OnInit {
   // items: Item[] = [];
   items: Observable<Item[]>;
 
-  @Input() category: string;
+  fullCategory: string;
+
+  @Input() shortCategory: string;
+  @Input() rainbow: number;
 
   constructor(private itemService: ItemService) {}
   ngOnInit() {
-    console.log(this.category);
+    switch (this.shortCategory) {
+      case "ant":
+        this.fullCategory = "Anthenas";
+        break;
+      case "bat":
+        this.fullCategory = "Batteries";
+        break;
+      case "cam":
+        this.fullCategory = "Cameras";
+        break;
+      case "esc":
+        this.fullCategory = "Electronic Speed Controers";
+        break;
+      case "fc":
+        this.fullCategory = "Flight Controlers";
+        break;
+      case "frm":
+        this.fullCategory = "Frames";
+        break;
+      case "mot":
+        this.fullCategory = "Motors";
+        break;
+      case "prop":
+        this.fullCategory = "Propellers";
+        break;
+      case "rx":
+        this.fullCategory = "Radio Receivers";
+        break;
+      case "tx":
+        this.fullCategory = "Radio Transmitters";
+        break;
+      case "vtx":
+        this.fullCategory = "Video Transmitters";
+        break;
+      default:
+      // code block
+    }
     this.items = this.itemService.getCameras();
   }
 }
