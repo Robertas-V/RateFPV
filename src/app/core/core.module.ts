41405fc878793510825266835fcff4fde8ca13c9
Robertas-V <robertas.visniakovas@gmail.com>
2020-03-04 23:00:52 +0200

    Added page not found

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { SearchComponent } from "./search/search.component";
+import { PageNotFound404Component } from './pages/page-not-found404/page-not-found404.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, SearchComponent],
+  declarations: [HeaderComponent, FooterComponent, SearchComponent, PageNotFound404Component],
   imports: [CommonModule, FontAwesomeModule],
   exports: [HeaderComponent, SearchComponent]
 })
